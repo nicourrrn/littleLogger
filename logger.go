915aff34bfb3abb9cfb4ef6e10606bbdce0d2ecb
Vadym Sorokin <nicourrrn@gmail.com>
2021-12-23 22:08:59 +0200

    Add structure Logger and New function

--- logger.go
+++ logger.go
@@ -1,17 +1,38 @@
 package littleLogger
 
 import (
-	"bufio"
+	"io"
 	"sync"
+	"fmt"
+	"errors"
 )
 
+
 type Logger struct {
-	target bufio.Writer
+	target io.Writer
 	mu sync.Mutex
+	levels [4]bool
 }
 
-func NewLogger(target bufio.Writer) *Logger {
-	return &Logger{target: target}
+//NewLogger lvls = {
+//	1 - debug
+//	2 - warning TODO Рассмотреть порядок еще раз
+//	3 - info
+//	4 - error
+//}
+func NewLogger(target io.Writer, lvls ...int) (*Logger, error) {
+	if len(lvls) > 4 {
+		return nil, errors.New("Count of lvls less than 5")
+	}
+	levels := [4]bool{false,true,true,true}
+	for i, l := range lvls{
+		levels[i] = l == 1
+	}
+	_, err := fmt.Fprint(target, "Starting logs")
+	if err != nil {
+		return nil, err
+	}
+	return &Logger{target: target, levels: levels}, nil
 }
 
 
